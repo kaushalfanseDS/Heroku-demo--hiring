c090cef1c6e539f2c404a68e322c26f75964b0ad
kaushalfanseDS <50509993+kaushalfanseDS@users.noreply.github.com>
2021-09-08 18:41:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask.helpers import send_from_directory
+import numpy as np
+import os
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'),encoding='latin1')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path,'static'),
+    'favicon.ico',mimetype='images/favicon.png') 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
